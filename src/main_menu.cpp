5ad788d2dea4c695db1307413c956971743a8bc6
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2023-11-05 00:23:42 +0700

    [MainMenu] add ui slicing choose game menu

--- src/main_menu.cpp
+++ src/main_menu.cpp
@@ -3,18 +3,57 @@
 MainMenu::MainMenu(sf::RenderWindow& wnd)
 	:
 	window(wnd),
-	title("The Game Collection", 24, sf::Color::White, sf::Vector2f(
+	title("The Game Collection", 36, sf::Color::White, sf::Vector2f(
 		window.getSize().x / 2,
-		window.getSize().y / 2
-	), window)
-{}
+		200
+	), window),
+	chooseGame("Choose Game to Play", 18, sf::Color::White, sf::Vector2f(), window)
+{
+	gameTitlesButtons.reserve(gameTitles.size());
+
+	for (std::string gameTitle : gameTitles)
+	{
+		gameTitlesButtons.emplace_back(
+			gameTitle, 20,
+			sf::Vector2f(
+				window.getSize().x / 2,
+				window.getSize().y / 2
+			),
+			sf::Vector2f(140.0f, 50.0f),
+			UI::Padding(0.0f, 0.0f),
+			sf::Color::White, sf::Color::Black,
+			sf::Color::Black, sf::Color::White,
+			1, sf::Color::White, sf::Color::White,
+			window
+		);
+	}
+}
 
 void MainMenu::Draw()
 {
 	DrawTitle();
+	DrawChooseMenu();
 }
 
 void MainMenu::DrawTitle()
 {
 	title.Draw();
+}
+
+void MainMenu::DrawChooseMenu()
+{
+	chooseGame.SetPosition(sf::Vector2f(
+		window.getSize().x / 2,
+		window.getSize().y / 2
+	));
+	chooseGame.Draw();
+
+	for (UI::Button button : gameTitlesButtons)
+	{
+		button.SetButtonPosition(sf::Vector2f(
+			window.getSize().x / 2,
+			window.getSize().y / 2 + 50
+		));
+		button.Draw();
+	}
 }
--- src/main_menu.hpp
+++ src/main_menu.hpp
@@ -3,6 +3,7 @@
 #include "ui.hpp"
 
 #include <SFML/Graphics.hpp>
+#include <vector>
 
 class MainMenu
 {
@@ -10,8 +11,13 @@ public:
 	MainMenu(sf::RenderWindow& wnd);
 	void Draw();
 	void DrawTitle();
+	void DrawChooseMenu();
 
 private:
 	sf::RenderWindow& window;
 	UI::Text title;
+	UI::Text chooseGame;
+	
+	std::vector<std::string> gameTitles = { "Tic-tac-toe" };
+	std::vector<UI::Button> gameTitlesButtons;
 };
