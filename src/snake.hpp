c38eb085148e9bc9cdf92d50d8acd9a196d5b4c3
Reyhan Farabi <64721326+reyhanfarabi@users.noreply.github.com>
2023-11-14 13:44:45 +0700

    [SNK] adjust move min period

--- src/snake.hpp
+++ src/snake.hpp
@@ -44,7 +44,7 @@ private:
 	UI::Text txtTitle;
 
 	float movePeriod = 0.4f;
-	float movePeriodMin = 0.02f;
+	float movePeriodMin = 0.1f;
 	float movePeriodReduceFactor = 0.02f;
 	float moveCounter = 0.0f;
 	const sf::Vector2i UP = sf::Vector2i(0, -1);
